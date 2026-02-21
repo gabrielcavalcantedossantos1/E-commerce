692499c98f04e2b4ef3cf18144acbef6d7cce632
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-02-21 16:36:51 -0300

    Componente Header finalizado

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,11 +4,15 @@ import { Link } from "react-router-dom";
 export function Header() {
   return (
     <header className="w-full px-1 bg-slate-200">
-      <nav>
-        <Link to="/">Dev Shop</Link>
-
-        <Link to="/cart">
+      <nav className="w-full max-w-7xl h-14 flex items-center px-5 justify-between mx-auto">
+        <Link to="/" className="font-bold text-2xl">
+          Dev Shop
+        </Link>
+        <Link to="/cart" className="relative">
           <FiShoppingCart size={24} color="#121212" />
+          <span className="absolute -right-3 -top-3 px-2.5 bg-sky-500 rounded-full w-6 h-6 flex items-center justify-center text-white text-xs">
+            2
+          </span>
         </Link>
       </nav>
     </header>
