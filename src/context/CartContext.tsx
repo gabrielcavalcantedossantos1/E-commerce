9a0801fbb3bf340a8ab971f7f4c71b2d4688cccc
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-02-22 13:19:13 -0300

    Criando o context api do projeto

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,8 +1,10 @@
 import { createContext, useState } from "react";
+import type { ProdutosProps } from "../pages/Home";
 
 interface CartContextData {
   cart: CartProps[];
   cartAmount: number;
+  addItemCart: (newItem: ProdutosProps) => void;
 }
 
 interface CartProps {
@@ -23,9 +25,32 @@ export const CartContext = createContext({} as CartContextData);
 
 function CartProvider({ children }: ChildrenProps) {
   const [cart, setCart] = useState<CartProps[]>([]);
+  function addItemCart(newItem: ProdutosProps) {
+    const indexItem = cart.findIndex((item) => item.id === newItem.id);
+
+    if (indexItem !== -1) {
+      // item já existe => incrementa
+      const cartList = [...cart]; // copia o array para não alterar o estado direto
+      cartList[indexItem].amount += 1;
+      cartList[indexItem].total =
+        cartList[indexItem].amount * cartList[indexItem].price;
+      setCart(cartList);
+      return;
+    }
+
+    // item não existe → adiciona novo
+    const data: CartProps = {
+      ...newItem,
+      amount: 1,
+      total: newItem.price,
+    };
+    setCart((prev) => [...prev, data]);
+  }
 
   return (
-    <CartContext.Provider value={{ cart, cartAmount: cart.length }}>
+    <CartContext.Provider
+      value={{ cart, cartAmount: cart.length, addItemCart }}
+    >
       {children}
     </CartContext.Provider>
   );
