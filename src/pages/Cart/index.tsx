ccde185b6c233e7215cd356e877e0da148a30b66
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-02-24 18:17:17 -0300

    Corrigindo erro de responsividade no carrinho de itens

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -25,7 +25,7 @@ export function Cart() {
         cart.map((item) => (
           <section
             key={item.id}
-            className="flex items-center justify-between border-b-2 border-gray-300"
+            className="flex flex-col gap-3 items-center justify-between border-b-2 border-gray-300 md:flex-row"
           >
             <img src={item.cover} alt={item.title} className="w-28" />
 
